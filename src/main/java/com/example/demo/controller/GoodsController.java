a2af96ddfcde8a3b38acf20d98ba61ea4b394368
beom324 <scarlet2010@nate.com>
2024-03-19 16:29:50 +0900

    상품목록완료[김범진]

--- src/main/java/com/example/demo/controller/GoodsController.java
+++ src/main/java/com/example/demo/controller/GoodsController.java
@@ -1,5 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GoodsController {
+	
+	@GetMapping("/listGoods")
+	public String list() {
+		return "상품목록";
+		
+	}
 
 }
