53fe2b91f74b97858d80c3facb07dbef9789a908
JH744 <dydekfflrl@naver.com>
2024-03-19 16:30:04 +0900

    "상품목록 추가"

--- src/main/java/com/example/demo/Controller/BoardController.java
+++ src/main/java/com/example/demo/Controller/BoardController.java
@@ -3,8 +3,9 @@ package com.example.demo.Controller;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BoardController {
 
 	
